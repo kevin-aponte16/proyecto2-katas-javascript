768c3dc4fdcdf4146a1c484a993570ad3a691bcb
kevin-aponte16 <kevin.fiucv@gmail.com>
2025-10-20 23:07:17 +0200

    nuevos

---
+++ ejercicios-basicos-avanzados/ejercicio-33.js
@@ -0,0 +1,23 @@
+const capitals = {
+ Spain: 'Madrid',
+ France: 'Paris',
+ Italy: 'Rome',
+ Germany: 'Berlin',
+ Portugal: 'Lisbon',
+ Poland: 'Warsaw',
+ Greece: 'Athens',
+ Austria: 'Vienna',
+ Hungary: 'Budapest',
+ Ireland: 'Dublin'
+};
+
+function getCapital(country) {
+  if (capitals[country]) {
+    return capitals[country];
+  } else {
+    return `No tenemos registrada la capital de `+ country;
+  }
+}
+
+
+
